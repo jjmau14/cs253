dd6b95711f4435e3e9a70b6fa4d118ea973cc3bf
jjmau14 <jjmau14@gmail.com>
2017-03-20 15:13:45 -0600

    U.h -- 3/20 3:13

--- hw5/U.h
+++ hw5/U.h
@@ -41,55 +41,61 @@ class U {
 		// Methods of Class 'U' --------------------------------
 		void readFile(std::string fileName){}
 
+		// Parse Properties file -- 
 		void propfile(std::string fileName){
-			clear_properties();
+			clear_properties();			// if propfile is called more than once, clear existing data
 			std::ifstream propfile;
-			propfile.open(fileName);
+			propfile.open(fileName);	// open property file
 			std::string line = "";
-			if (!propfile.is_open())
+			if (!propfile.is_open())	// throw error if could not open property file
 				throw "Properties file could not be opened!";
-			while(std::getline(propfile, line)){
-				std::string temp_string = "";
+
+			while(std::getline(propfile, line)){	// Read property file line by line
+				std::string temp_string = "";	
 				int hex_value = 0;
 				int semi_colon_count = 0;
 				    for (char c : line){
 				        if (c == ';')
 				            break;
 				        else
-				            temp_string += c;
+				            temp_string += c;		// store hex code as string until first semicolon
 				    }
-					std::istringstream iss(temp_string);
+					std::istringstream iss(temp_string);	// ISS -- istringstream, parse string to hex code for storage
 					iss >> std::hex >> hex_value;
-					temp_string = "";
-				    for (char c : line){
-				        if (c == ';')
+					temp_string = "";						// reset temp_string for new data (property name)
+				    for (char c : line){					// Loop through line
+				        if (c == ';')						// Read data starting at semicolon 2 and ending at 3 (property names ie. 'Lu')
 				            semi_colon_count++;
-				        else if (semi_colon_count == 2)
+				        else if (semi_colon_count == 2)		// If not hit 3rd semicolon yet, keep adding char to string
 				            temp_string += c;
-				        else if(semi_colon_count > 2)
+				        else if(semi_colon_count > 2)		// On third semicolon, break and add to the following data members
 				            break;
 				    }
-				    utf_char_prop[hex_value] = temp_string;
-				    propNames.insert(temp_string);
+				    utf_char_prop[hex_value] = temp_string;		// Maps hex code to a property value (ie. 0x0066 mapped to Lu -- 'A')
+				    propNames.insert(temp_string);				// Adds property names to set (not multiset. aka add unique names only)
 			}
 			for (auto property_name : propNames){
-				prop_counts[property_name] = 0;
+				prop_counts[property_name] = 0;					// Init all properties to count of 0
 			}
-			propfile.close();
+			propfile.close();		// Close property file
 		}
-
+		
+		// return size (stored per data object in private vars)
 		int size() { return utf_size; }
 
-		std::string get(){return "";}
+		std::string get(){return utf_string;}
 		std::string get(int index){return "";}
 		std::string get(int start, int end){return "";}
-
+		
+		// Loops through all property counts until it finds the method
+		// 	parameter or hits the end of the map.
 		int propcount(std::string property){
-			if (prop_counts.find(property) != prop_counts.end())
-				return prop_counts.find(property)->second;
-			return 0;
+			if (prop_counts.find(property) != prop_counts.end())	// if passed prop name == current item in map
+				return prop_counts.find(property)->second;			// return that property->second (first = name; second = count)
+			return 0;												// else not found (aka return 0)
 		}
-
+		
+		// Returns set of all property names
 		std::set<std::string> props(){return propNames;}
 
 	private:
