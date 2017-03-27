1758c18c09d4a376044f0c49e8c5a2f13eb919d4
jjmau14 <jjmau14@gmail.com>
2017-03-27 08:46:39 -0600

    Assignment fixed (Added test for assignment ctor)

--- hw5/main.cc
+++ hw5/main.cc
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]){
 		test.readfile(argv[2]);
                 
                 U copy_test(test);
-                U x = copy_test;
+                U &assign_test = copy_test;
                 
 		cout << "Number of Lu: " << test.propcount("Lu") << endl;
 		cout << "Size: " << test.size() << endl;
@@ -27,6 +27,10 @@ int main(int argc, char* argv[]){
                 cout << "Number of Lu: " << copy_test.propcount("Lu") << endl;
 		cout << "Size: " << copy_test.size() << endl;
 		cout << "At index 9: " << copy_test.get(4,copy_test.size()) << endl;
+                
+                cout << "Number of Lu: " << assign_test.propcount("Lu") << endl;
+		cout << "Size: " << assign_test.size() << endl;
+		cout << "At index 9: " << assign_test.get(4,assign_test.size()) << endl;
                 
 	}catch (const string error){
 		cout << error << endl;
