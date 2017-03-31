67c2fe47eab6375546da8f4e4c2f6ce36f595424
jjmau14 <jjmau14@gmail.com>
2017-03-31 09:37:54 -0600

    New test cases

--- hw6/main.cc
+++ hw6/main.cc
@@ -7,14 +7,15 @@ using namespace std;
 int main(){
     try {
         U u;
+        u.readfile("data");
         u.append("a³+b³≠c³");
         P p("UnicodeData.txt");
         for (int i = 0 ; i < u.size() ; i++)
             p.count(u.codepoint(i));
-        cout << "should be 8: " << u.size() << '\n'
-            << "Should be 2: " << p.count("Sm") << '\n'
-            << "Should be b³: " << u.get(3,5) << "\n\n";
-    }catch(...){
-        cout << "caught an error!" << endl;
+        cout << "Should be 17: " << u.size() << '\n'
+            << "Should be 1: " << p.count("Lu") << '\n'
+            << "Accumulated String: " << u.get() << "\n\n";
+    }catch(const string error){
+        cout << error << endl;
     }
 }
