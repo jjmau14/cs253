5e5432ec6b7ffc34acc1f3d22ac7fd056331cc13
jjmau14 <jjmau14@gmail.com>
2017-04-29 09:16:51 -0600

    yet again, more test cases

--- hw7/main.cc
+++ hw7/main.cc
@@ -18,22 +18,22 @@ int main(){
 		if (u.size() == 9)
 			cout << "Method U::size() : test passed" << endl;
 		else 
-			cout << "Method U::size() : test failed" << endl;
+			cout << "Method U::size() : test FAILED" << endl;
 		
 		if (p.count("Lu")  == 1)
 			cout << "Method P::count(""Lu"") : test passed" << endl;
 		else
-			cout << "Method P::count(""Lu"") : test failed" << endl;
+			cout << "Method P::count(""Lu"") : test FAILED" << endl;
 
 		if (p.size() == 29)
 			cout << "Method P::size() : test passed" << endl;
 		else
-			cout << "Method P::size() : test failed" << endl;
+			cout << "Method P::size() : test FAILED" << endl;
 		
 		if (u.get() == "a 5 Ω ䷊ 🐮")
 			cout << "Method U::get() : test passed" << endl;
 		else
-			cout << "Method U::get() : test failed\n"
+			cout << "Method U::get() : test FAILED\n"
 			"Maybe a newline at the end of data?" << endl;
 			
 		p.clear();
@@ -46,42 +46,61 @@ int main(){
 		if (u_copy.size() == 9 && p.count("Ll") == 1 && p.size() == 29 && u_copy.get() == "a 5 Ω ䷊ 🐮")
 			cout << "Copy Constructor : test passed" << endl;
 		else
-			cout << "Copy Constructor : test failed" << endl;
+			cout << "Copy Constructor : test FAILED" << endl;
 			
 		const U const_copy(u);
 		u = string("☮☮☮");
 		U u2 = string("☮☮☮");
 		
+		U u3 = u + u2;
+		U u4 = u + string("☮☮☮");
+		
+		if (u3.size() == 6 && (u3.get() == u.get() + u2.get()))
+			cout << "U::operator+(U) : test passed" << endl;
+		else
+			cerr << "U::operator+(U) : test FAILED" << endl;
+		
+		if (u4.size() == 6 && (u3.get() == u.get() + string("☮☮☮")))
+			cout << "U::operator+(std::string) : test passed" << endl;
+		else
+			cerr << "U::operator+(std::string) : test FAILED" << endl;
+		
 		if (u == u2)
 			cout << "U::operator==(U) : test passed" << endl;
 		else
-			cerr << "U::operator==(U) : test failed" << endl;
+			cerr << "U::operator==(U) : test FAILED" << endl;
 		
 		if (u != u2) // should be false
-			cout << "U::operator!=(u) : test failed" << endl;
+			cout << "U::operator!=(U) : test FAILED" << endl;
 		else
-			cout << "U::operator!=(u) : test passed" << endl;
+			cout << "U::operator!=(U) : test passed" << endl;
+		
+		const U u5 = string("abc");
+		if ( u5[0] == "a" && u5[2] == "c")
+			cout << "U::operator[](int) : test passed" << endl;
+		else
+			cout << "U::operator[](int) : test FAILED; maybe method is not const?" << endl;
 		
 		if (u == "☮☮☮")
-			cout << "U::operator==(string) : test passed" << endl;
+			cout << "U::operator==(std::string) : test passed" << endl;
 		else
-			cout << "U::operator==(string) : test failed" << endl;
+			cout << "U::operator==(std::string) : test FAILED" << endl;
 		
 		if (u == "☮☮") // should be false
-			cout << "U::operator!=(string) : test failed" << endl;
+			cout << "U::operator!=(std::string) : test FAILED" << endl;
 		else
-			cout << "U::operator!=(string) : test passed" << endl;
+			cout << "U::operator!=(std::string) : test passed" << endl;
 		
 		P p2;
 		if (!p2) 
 			cout << "P::operator bool() returns true: test passed" << endl;
 		else 
-			cout << "P::operator bool() : test failed" << endl;
+			cout << "P::operator bool() : test FAILED" << endl;
 		
 		p2.readfile("UnicodeData.txt");
 		
 		if (!p2) // should be false
-			cout << "P::operator bool() returns false: test failed" << endl;
+			cout << "P::operator bool() returns false: test FAILED" << endl;
 		else 
 			cout << "P::operator bool() returns false: test passed" << endl;
 		
